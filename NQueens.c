b87fbe1a67a0b358b5a413e53f42d7ce11a82a97
toshniwal-keshav <keshavtoshniwal.cs24@bmsce.ac.in>
2026-06-02 06:33:24 +0530

    Update NQueens to count and display solutions
    Added a solution counter and total solutions output.

--- NQueens.c
+++ NQueens.c
@@ -2,24 +2,25 @@
 #include <stdlib.h>
 
 int x[20];
+int sol = 0;
 
-int place(int k, int i) {
-    for (int j = 1; j < k; j++) {
+int place(int k, int i){
+    for (int j = 1; j < k; j++)
         if (x[j] == i || abs(j - k) == abs(x[j] - i))
             return 0;
-    }
     return 1;
 }
 
-void NQueens(int k, int n) {
-    for (int i = 1; i <= n; i++) {
-        if (place(k, i)) {
+void NQueens(int k, int n){
+    for (int i = 1; i <= n; i++)    {
+        if (place(k, i)){
             x[k] = i;
+            if (k == n){
+                sol++;
+                printf("\nSolution %d:\n", sol);
 
-            if (k == n) {
-                // Print solution as a binary matrix
                 for (int row = 1; row <= n; row++) {
-                    for (int col = 1; col <= n; col++) {
+                    for (int col = 1; col <= n; col++){
                         if (x[row] == col)
                             printf("1 ");
                         else
@@ -27,22 +28,19 @@ void NQueens(int k, int n) {
                     }
                     printf("\n");
                 }
-                printf("\n");
             }
-            else {
+            else
                 NQueens(k + 1, n);
-            }
+            
         }
     }
 }
 
-int main() {
+int main(){
     int n;
-
     printf("Enter number of queens: ");
     scanf("%d", &n);
-
     NQueens(1, n);
-
+    printf("\nTotal solutions = %d\n", sol);
     return 0;
 }
